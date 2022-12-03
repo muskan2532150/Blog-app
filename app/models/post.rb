b69867625d50f090b21f6d812fe1de6188f08937
muskan2532150 <rg2532150@gmail.com>
2022-12-03 18:33:45 +0530

    Fixed rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
-  validates :title, length: {maxmium:250},presence:true, allow_blank:false
-  validates_numericality_of :comment_counter, only_integer:true, presence:true,  greater_than_or_equal_to: 0
-  validates_numericality_of :likes_counter, only_integer:true, presence:true,  greater_than_or_equal_to: 0
+  validates :title, length: { maxmium: 250 }, presence: true, allow_blank: false
+  validates_numericality_of :comment_counter, only_integer: true, presence: true, greater_than_or_equal_to: 0
+  validates_numericality_of :likes_counter, only_integer: true, presence: true, greater_than_or_equal_to: 0
 
   def update_posts_count
     author.increment!(:posts_counter)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
 
-  validates :name, presence:true, allow_blank:false
-  validates_numericality_of :posts_counter, only_integer:true, presence:true,  greater_than_or_equal_to: 0
+  validates :name, presence: true, allow_blank: false
+  validates_numericality_of :posts_counter, only_integer: true, presence: true, greater_than_or_equal_to: 0
 
   def three_recent_posts
     posts.where(author_id: id).last(3)
