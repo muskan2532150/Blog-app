2ddf527b4738e3f813d1b4f12b2ba563be9a285b
muskan2532150 <rg2532150@gmail.com>
2022-12-03 16:35:09 +0530

    Fixed rubocop

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
-    after_save :update_likes_count
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_save :update_likes_count
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   def update_likes_count
     post.increment!(:likes_counter)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :likes, foreign_key: 'author_id'
-    has_many :comments, foreign_key: 'author_id'
-    has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
 
-    def three_recent_posts
-        posts.where(author_id: id).last(3)
-      end
+  def three_recent_posts
+    posts.where(author_id: id).last(3)
+  end
 end
