fa0c7e397c161172253b511c6ddfb28d1eeabab1
muskan2532150 <rg2532150@gmail.com>
2022-12-03 21:07:52 +0530

    Added unit test cases

---
+++ spec/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  before do
+    @user = User.new(name: 'Muskan', posts_counter: 1)
+    @post = Post.new(author: @user, title: 'Sakshi', comment_counter: 1, likes_counter: 1)
+  end
+
+  it 'is valid without a title' do
+    comment =  Comment.new(text: 'test', author_id: @user.id, post_id: @post.id)
+    expect(comment).to_not be_valid
+  end
+
+  it 'is not valid if without author id' do
+    comment = Comment.new(author_id: nil)
+    expect(comment).to_not be_valid
+  end
+end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -15,11 +15,23 @@ describe Post, User do
     expect(post).to_not be_valid
   end
 
-  it 'is not valid if comments counter is not interger' do
+  it 'is not valid if likes counter is not interger' do
     post = Post.new(likes_counter: nil)
     expect(post).to_not be_valid
   end
+  
+  it 'is not valid with a title longer than 250 characters' do
+    post = Post.new(title: 'ddd')
+    post.title = 'ddd' * 251
+    expect(post).to_not be_valid
+  end
 
+  it 'is not valid with a comments_counter being less than 0' do
+    post = Post.new(comment_counter: 0)
+    post.comment_counter = -1
+    expect(post).to_not be_valid
+  end
+  
   it 'is valid if title is string and comments counter & likes counter is integer' do
     post = Post.new(author: @user, title: 'Sakshi', comment_counter: 1, likes_counter: 1)
     expect(post).to be_valid
