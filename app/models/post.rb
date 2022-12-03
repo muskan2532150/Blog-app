0b47c55804b7e8e9bef1ceca2711d3bf0c1e00cd
muskan2532150 <rg2532150@gmail.com>
2022-12-03 16:30:24 +0530

    Updated models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    after_save :update_posts_count
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_many :comments, foreign_key: 'post_id'
+    has_many :likes, foreign_key: 'post_id'
+
+    def update_posts_count
+        author.increment!(:posts_counter)
+      end
+    
+      def five_recent_comments
+        Comment.where(post_id: id).last(5)
+      end
 end
