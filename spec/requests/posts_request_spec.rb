781ec8915ccb9f07a5b76b1c9a08a107c0cbae9a
muskan2532150 <rg2532150@gmail.com>
2022-12-16 15:09:31 +0530

    Fixed the naming for controller

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -3,8 +3,25 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/:user_id/posts/'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render index' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should render show' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template(:show)
     end
   end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -6,5 +6,22 @@ RSpec.describe 'Users', type: :request do
       get '/user/index'
       expect(response).to have_http_status(:success)
     end
+
+    it 'should render index' do
+      get '/'
+      expect(response).to render_template(:index)
+    end
+  end
+end
+
+describe 'GET /show' do
+  it 'returns http success' do
+    get '/users/:id'
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'should render show' do
+    get '/users/:id'
+    expect(response).to render_template(:show)
   end
 end
