113eece7c8e0c798bfc3b85b27eb8e567893e4d1
muskan2532150 <rg2532150@gmail.com>
2022-12-16 15:34:56 +0530

    Add unit case for posts

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -2,25 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/:user_id/posts/'
+   before do 
+     get '/users/:user_id/posts/' 
+   end
+   
+     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
     it 'should render index' do
-      get '/users/:user_id/posts/:id'
       expect(response).to render_template(:index)
     end
   end
 
   describe 'GET /show' do
+   before do 
+     get '/users/:user_id/posts/:id'
+   end
+
     it 'returns http success' do
-      get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
     end
 
     it 'should render show' do
-      get '/users/:user_id/posts/:id'
       expect(response).to render_template(:show)
     end
   end
