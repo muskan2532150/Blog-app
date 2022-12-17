c63d3afe50dcf6d615f61a3e6d798f3a1c9ceb1b
muskan2532150 <rg2532150@gmail.com>
2022-12-17 22:10:30 +0530

    Add new,create action for like

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'likes/new'
   root "users#index"
   
   resources :users ,only:[:index,:show]do
     resources :posts ,only:[:index,:show, :new,:create] do
       resources :comments ,only:[:new, :create]
+      resources :likes ,only:[:new,:create]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
