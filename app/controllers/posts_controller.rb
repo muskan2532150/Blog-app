322228b62bf3223ffd7bf8c2719186ebb789d882
muskan2532150 <rg2532150@gmail.com>
2022-12-17 21:35:02 +0530

    Add form file, new,create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,27 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_post_path(current_user, @post)
+        else
+          redirect_to new_user_post_path(current_user)
+        end
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(author_id: current_user.id, comment_counter: 0, likes_counter: 0)
+  end
+
 end
