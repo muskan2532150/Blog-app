039d0b3b80523f0b0333d3e6c2f1c799985b52d0
muskan2532150 <rg2532150@gmail.com>
2022-12-23 21:07:27 +0530

    Add default values

--- db/migrate/20221203065119_create_users.rb
+++ db/migrate/20221203065119_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.text :bio
-      t.integer :posts_counter
+      t.string :photo, default: 'https://cdn.justjaredjr.com/wp-content/uploads/headlines/2017/02/selena-gomez-teases-new-song-listen-now.jpg'
+      t.text :bio, default: 'This is bio section'
+      t.integer :posts_counter, default:0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_051056) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "comment_counter"
-    t.integer "likes_counter"
+    t.integer "comment_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
@@ -46,9 +46,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_051056) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.integer "posts_counter"
+    t.string "photo", default: "https://cdn.justjaredjr.com/wp-content/uploads/headlines/2017/02/selena-gomez-teases-new-song-listen-now.jpg"
+    t.text "bio", default: "This is bio section"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
